46fb7d2d015012ab91922c38c67d3cc8f2411ee3
fy <fy0@qq.com>
2018-01-20 23:59:47 +0800

    fix

--- backend/model/notif.py
+++ backend/model/notif.py
@@ -1,6 +1,8 @@
+import time
 from peewee import *
 from playhouse.postgres_ext import ArrayField, BinaryJSONField
 
+import config
 from model import BaseModel, MyTimestampField, db
 from model.comment import COMMENT_STATE
 from model.topic import TOPIC_STATE
@@ -26,13 +28,14 @@ def fetch_notif_of_comment(user_id, last_comment_id=b'\x00'):
     # 某某 评论了你的文章 某某某： XXXXXX
     # 这个暂时不折叠了，全部显示在提醒中
     cur = db.execute_sql('''
-        SELECT "c".time, "c"."id", "c"."related_id", "c"."related_type", "c".user_id, 
+        SELECT %s, "c".time, "c"."id", "c"."related_id", "c"."related_type", "c".user_id, 
           "t"."title", left("c".content, 50)
-        FROM topic as t, comment as c
-        WHERE t.user_id = %s and t.state >= %s and
-          t.id = c.related_id and c.id > %s and c.state >= %s
-        ''', (user_id, TOPIC_STATE.CLOSE, last_comment_id, COMMENT_STATE.NORMAL))
-    # 时间，评论ID，文章ID，POST类型，用户ID，文章标题，前50个字
+        FROM topic AS t, comment AS c
+        WHERE t.user_id = %s AND t.state >= %s AND
+          t.id = c.related_id AND c.id > %s AND c.state >= %s
+        ORDER BY "c"."id" DESC
+        ''', (NOTIF_TYPE.BE_COMMENTED, user_id, TOPIC_STATE.CLOSE, last_comment_id, COMMENT_STATE.NORMAL))
+    # 类型，时间，评论ID，文章ID，POST类型，用户ID，文章标题，前50个字
     return cur.fetchall()
 
 
@@ -40,13 +43,14 @@ def fetch_notif_of_reply(user_id, last_reply_id=b'\x00'):
     # 某某 在文章 某某某 中回复了你的评论： XXXXXX
     # c2 是 user_id 的原评论，c 是回复评论的评论
     cur = db.execute_sql('''
-        SELECT "c".time, "c"."id", "c"."related_id", "c"."related_type", "c".user_id, 
+        SELECT %s, "c".time, "c"."id", "c"."related_id", "c"."related_type", "c".user_id, 
           "t"."title", left("c".content, 50)
         FROM topic AS t, comment AS c, comment AS c2
         WHERE c2.user_id = %s AND c2.state >= %s AND
-          c2.id = c.reply_to_cmt_id AND c.id > %s AND c.state >= %s AND t.id == c.related_id
-        ''', (user_id, COMMENT_STATE.NORMAL, last_reply_id, COMMENT_STATE.NORMAL))
-    # 时间，评论ID，文章ID，POST类型，用户ID，文章标题，前50个字
+          c2.id = c.reply_to_cmt_id AND c.id > %s AND c.state >= %s AND t.id = c.related_id
+        ORDER BY "c"."id" DESC
+        ''', (NOTIF_TYPE.BE_REPLIED, user_id, COMMENT_STATE.NORMAL, last_reply_id, COMMENT_STATE.NORMAL))
+    # 序号，时间，评论ID，文章ID，POST类型，用户ID，文章标题，前50个字
     return cur.fetchall()
 
 
@@ -62,13 +66,20 @@ class UserNotifRecord(BaseModel):
     last_sysmsg_id = BlobField(default=b'\x00')
     update_time = MyTimestampField(index=True)
 
-    def get_notifications(self):
+    def get_notifications(self, update_last=False):
         lst = []
         l1 = fetch_notif_of_comment(self.id, self.last_comment_id)
         l2 = fetch_notif_of_reply(self.id, self.last_reply_id)
         lst.extend(l1)
         lst.extend(l2)
-        lst.sort(key = lambda x: x[0])
+        lst.sort(key = lambda x: x[1], reverse=True)
+
+        if update_last:
+            if l1: self.last_comment_id = l1[0][2]
+            if l2: self.last_reply_id = l2[0][2]
+            self.update_time = int(time.time())
+            self.save()
+
         return lst
 
     class Meta:
@@ -84,5 +95,22 @@ class Notification(BaseModel):
     data = BinaryJSONField()
     is_read = BooleanField(default=False)
 
+    @classmethod
+    def refresh(cls, user_id):
+        new = []
+        r: UserNotifRecord = UserNotifRecord.get_by_pk(user_id)
+        for i in r.get_notifications():
+            if i[0] == NOTIF_TYPE.BE_COMMENTED:
+                new.append((config.ID_GENERATOR(), (), user_id, NOTIF_TYPE.BE_COMMENTED, i[1], i, False))
+        cls.insert_many(new)
+
     class Meta:
         db_table = 'notif'
+
+
+if __name__ == '__main__':
+    u: User = User.select().where(User.nickname == '折影').get()
+    r: UserNotifRecord = UserNotifRecord.get_by_pk(u.id)
+    for i in r.get_notifications():
+        print(i)
+    Notification.refresh(u.id)
---
+++ backend/view/notif.py
@@ -0,0 +1,36 @@
+import time
+import config
+from typing import Dict
+from model.notif import Notification
+from model.statistic import statistic_add_comment
+from model.topic import Topic
+from slim.utils.customid import CustomID
+from model.comment import Comment
+from model.post import POST_TYPES
+from slim.base.view import ParamsQueryInfo
+from slim.retcode import RETCODE
+from slim.support.peewee import PeeweeView
+from slim.utils import to_bin
+from view import route, ValidateForm
+from wtforms import validators as va, StringField, IntegerField, ValidationError
+from view.user import UserMixin
+
+
+@route('notif')
+class NotificationView(UserMixin, PeeweeView):
+    model = Notification
+
+    @classmethod
+    def interface(cls):
+        cls.discard('new')
+        cls.discard('set')
+        cls.discard('delete')
+
+    @classmethod
+    def ready(cls):
+        # cls.add_soft_foreign_key('user_id', 'user')
+        # cls.add_soft_foreign_key('reply_to_cmt_id', 'comment')
+        pass
+
+    def refresh(self):
+        pass
