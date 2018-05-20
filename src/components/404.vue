cc381d919e569abbf27a6f129f500ed32e223b99
fy <fy0@qq.com>
2018-05-21 02:01:44 +0800

    修复一个因为页面加载太慢被备选的404页面的倒计时器跳到首页的问题

--- src/components/404.vue
+++ src/components/404.vue
@@ -47,6 +47,8 @@ export default {
     methods: {
         fetchData: function () {
             this.timer = setInterval(() => {
+                // 防止某些页面因为网络太慢，在还未加载出内容时被404页面倒计时跳转主页
+                if (state.loading) return
                 this.second --
                 if (!this.second) {
                     clearInterval(this.timer)
