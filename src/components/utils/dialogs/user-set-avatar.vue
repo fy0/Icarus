570fdd69f55cf354667a9b09d9d26e7acc4965a9
fy <fy0@qq.com>
2018-06-11 23:55:41 +0800

    解决全局scss变量问题

--- src/components/utils/dialogs/user-set-avatar.vue
+++ src/components/utils/dialogs/user-set-avatar.vue
@@ -1,13 +1,25 @@
 <template>
 <ic-dialog :show="state.dialog.userSetAvatar" :on-close="close">
-    <div class="rect">点击或拖动图片至此处</div>
-    <button class="ic-btn primary">取消</button>
+    <template v-if="!image">
+        <input type="file" ref="inputFile" @change="onFileChange" accept="image/*" class="input-file" />
+        <div @click="selectFile" class="rect">点击或拖动图片至此处</div>
+        <button class="ic-btn primary">取消</button>
+    </template>
+    <template v-else>
+        <img :src="image" />
+        <img :src="image2" />
+        <img :src="image2" />
+    </template>
 </ic-dialog>
 </template>
 
 <style lang="scss" scoped>
+.input-file {
+    display: none;
+}
+
 .rect {
-    background-color: #eee;
+    background-color: $gray-200;
     padding: 30px;
 }
 </style>
@@ -18,10 +30,27 @@ import state from '@/state.js'
 export default {
     data () {
         return {
-            state
+            state,
+            image: '',
+            image2: ''
         }
     },
     methods: {
+        selectFile: async function () {
+            this.$refs.inputFile.click()
+        },
+        onFileChange: async function (e) {
+            let files = e.target.files || e.dataTransfer.files
+            if (!files.length) return
+            this.createImage(files[0])
+        },
+        createImage: function (file) {
+            let reader = new FileReader()
+            reader.onload = (e) => {
+                this.image = e.target.result
+            }
+            reader.readAsDataURL(file)
+        },
         close: function () {
             state.dialog.userSetAvatar = false
         }
