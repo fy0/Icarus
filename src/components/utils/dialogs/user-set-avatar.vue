15114850e4274c850fc5922e6f23833bfd84af55
fy <fy0@qq.com>
2018-06-12 23:58:06 +0800

    input range 样式

--- src/components/utils/dialogs/user-set-avatar.vue
+++ src/components/utils/dialogs/user-set-avatar.vue
@@ -1,26 +1,63 @@
 <template>
 <ic-dialog :show="state.dialog.userSetAvatar" :on-close="close">
-    <template v-if="!image">
-        <input type="file" ref="inputFile" @change="onFileChange" accept="image/*" class="input-file" />
-        <div @click="selectFile" class="rect">点击或拖动图片至此处</div>
-        <button class="ic-btn primary">取消</button>
-    </template>
-    <template v-else>
-        <img :src="image" />
-        <img :src="image2" />
-        <img :src="image2" />
-    </template>
+    <div class="content">
+        <template v-if="!image">
+            <div class="up">
+                <input type="file" ref="inputFile" @change="onFileChange" accept="image/*" class="input-file" />
+                <div @click="selectFile" class="rect">点击或拖动图片至此处</div>
+            </div>
+            <div class="down">
+                <button class="ic-btn primary">取消</button>
+            </div>
+        </template>
+        <template v-else>
+            <div class="up">
+                <div class="img-container">
+                    <img :src="image" />
+                </div>
+                <div class="vicp-range">
+                    <input class="ic-input primary" type="range" step="1" min="0" max="100" value="68">
+                    <i class="vicp-icon5"></i>
+                    <i class="vicp-icon6"></i>
+                </div>
+                <img :src="image2" />
+                <img :src="image2" />
+            </div>
+            <div class="down">
+                <button class="ic-btn primary">取消</button>
+            </div>
+        </template>
+    </div>
 </ic-dialog>
 </template>
 
 <style lang="scss" scoped>
+.content {
+    display: flex;
+    flex-direction: column;
+
+    .down {
+        margin-top: 20px;
+        display: flex;
+        justify-content: flex-end;
+    }
+}
+
+.img-container {
+    width: 240px;
+    height: 180px;
+    overflow: hidden;
+}
+
 .input-file {
     display: none;
 }
 
 .rect {
+    text-align: center;
     background-color: $gray-200;
-    padding: 30px;
+    padding: 80px 0;
+    margin-bottom: 60px;
 }
 </style>
 
