db0c30c3425de76ec741d36934487b7e39aedd3c
fy <fy0@qq.com>
2018-08-03 02:27:17 +0800

    tab 机制实现

---
+++ src/components/utils/ui/tab.vue
@@ -0,0 +1,48 @@
+<template>
+<div class="ic-tab" :class="[isActive ? 'active' : '']" @click="doActive">
+    <span class="text">{{title}}</span>
+</div>
+</template>
+
+<style lang="scss" scoped>
+.ic-tab {
+    user-select: none;
+    outline: none;
+    cursor: pointer;
+
+    .text {
+        padding: 10px 20px;
+    }
+}
+</style>
+
+<script>
+import state from '@/state.js'
+
+export default {
+    props: {
+        title: {
+            type: String
+        },
+        value: {
+            type: String
+        }
+    },
+    data () {
+        return {
+            state
+        }
+    },
+    computed: {
+        isActive: function () {
+            return this.getCurrentActive() === this.value
+        }
+    },
+    inject: ['setActive', 'getCurrentActive'],
+    methods: {
+        doActive: function () {
+            this.setActive(this.value)
+        }
+    }
+}
+</script>
---
+++ src/components/utils/ui/tabs.vue
@@ -0,0 +1,44 @@
+<template>
+<div class="ic-tabs">
+    <slot />
+</div>
+</template>
+
+<style>
+.ic-tabs {
+    display: flex;
+}
+</style>
+
+<script>
+import state from '@/state.js'
+
+export default {
+    props: {
+        value: {
+            type: String
+        }
+    },
+    data () {
+        return {
+            state
+        }
+    },
+    provide () {
+        return {
+            'setActive': this.setActive,
+            'getCurrentActive': this.getCurrentActive
+        }
+    },
+    mounted: async function () {
+    },
+    methods: {
+        setActive: function (name) {
+            this.$emit('input', name)
+        },
+        getCurrentActive: function () {
+            return this.value
+        }
+    }
+}
+</script>
