23dd84c6cbdaf4ad5b323e77931dfb47d8212f7b
fy <fy0@qq.com>
2018-10-07 23:22:14 +0800

    切换到 font-awesome5 并按需加载

--- src/main.js
+++ src/main.js
@@ -9,15 +9,45 @@ import 'normalize.css'
 import 'qiniu-js'
 import 'animate.css'
 import 'nprogress/nprogress.css'
-import 'font-awesome/css/font-awesome.css'
 
 Vue.config.productionTip = false
+
+// 加载动画组件
 
 import 'vue-loaders/dist/vue-loaders.css'
 import * as VueLoaders from 'vue-loaders'
 Vue.use(VueLoaders)
 
+// fontawesome
+// P.S. faH1-HX 以及 faRepeat 现在都是 pro 才能用了
+
+import { library, dom } from '@fortawesome/fontawesome-svg-core'
+import { faArrowUp, faArrowsAlt, faBold, faCode, faColumns, faEraser, faEye, faHeading, faImage, faItalic, faLink, faListOl, faListUl, faMinus, faQuestionCircle, faQuoteLeft, faStrikethrough, faTable, faUndo } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faArrowUp)
+library.add(faArrowsAlt)
+library.add(faBold)
+library.add(faCode)
+library.add(faColumns)
+library.add(faEraser)
+library.add(faEye)
+library.add(faHeading)
+library.add(faImage)
+library.add(faItalic)
+library.add(faLink)
+library.add(faListOl)
+library.add(faListUl)
+library.add(faMinus)
+library.add(faQuestionCircle)
+library.add(faQuoteLeft)
+library.add(faStrikethrough)
+library.add(faTable)
+library.add(faUndo)
+
+dom.watch()
+
 // social share
+
 import 'social-share.js/dist/js/social-share.min.js'
 import 'social-share.js/dist/css/share.min.css'
 
